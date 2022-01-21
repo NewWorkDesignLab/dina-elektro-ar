4656086d514f70d4052339858fa1cc5b0f5cec61
Tobias Bohn <info@tobiasbohn.com>
2022-01-21 10:26:33 +0100

    plane tracking and dotted lines

--- Assets/Project/Scripts/InputManager.cs
+++ Assets/Project/Scripts/InputManager.cs
@@ -4,34 +4,34 @@ using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
-// [RequireComponent(typeof(ARRaycastManager))]
+[RequireComponent(typeof(ARRaycastManager))]
 public class InputManager : MonoBehaviour
 {
     public GameObject target;
-    // private ARRaycastManager raycastManager;
+    private ARRaycastManager raycastManager;
  
-    // static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
  
-    // private void Awake()
-    // {
-    //     raycastManager = GetComponent<ARRaycastManager>();
-    // }
+    private void Awake()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+    }
  
     void Update()
     {
         if (Input.touchCount == 0)
             return;
 
-        // if (raycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.All))
-        // {
-        //     var hitPose = hits[0].pose;
-        //     Instantiate(target, hitPose.position, hitPose.rotation);
-        // }
-
-        RaycastHit hit;
-        if (Physics.Raycast(Input.GetTouch(0).position, transform.TransformDirection(Vector3.forward), out hit))
+        if (raycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.PlaneWithinPolygon))
         {
-            Instantiate(target, hit.point, Quaternion.identity);
+            var hitPose = hits[0].pose;
+            Instantiate(target, hitPose.position, hitPose.rotation);
         }
+
+        // RaycastHit hit;
+        // if (Physics.Raycast(Input.GetTouch(0).position, transform.TransformDirection(Vector3.forward), out hit))
+        // {
+        //     Instantiate(target, hit.point, Quaternion.identity);
+        // }
     }
 }
